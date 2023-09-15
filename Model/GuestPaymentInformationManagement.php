078c49e7464a980862d95cfa1a0a83609069418d
vitornicchio <vitor@imaginationmedia.com>
2023-09-15 13:28:00 -0300

    Release bugfixes

--- Model/GuestPaymentInformationManagement.php
+++ Model/GuestPaymentInformationManagement.php
@@ -44,29 +44,17 @@ class GuestPaymentInformationManagement extends MGuestPaymentInformationManageme
         \Magento\Checkout\Api\PaymentInformationManagementInterface $paymentInformationManagement,
         \Magento\Quote\Model\QuoteIdMaskFactory $quoteIdMaskFactory,
         CartRepositoryInterface $cartRepository,
-        LoggerInterface $logger,
-        \Magento\Framework\App\ProductMetadataInterface $productMetadata
+        LoggerInterface $logger
     ) {
-        if (version_compare($productMetadata->getVersion(), '2.4.6', '>=')) {
-            parent::__construct(
-                $billingAddressManagement,
-                $paymentMethodManagement,
-                $cartManagement,
-                $paymentInformationManagement,
-                $quoteIdMaskFactory,
-                $cartRepository,
-                $logger
-            );
-        } else {
-            parent::__construct(
-                $billingAddressManagement,
-                $paymentMethodManagement,
-                $cartManagement,
-                $paymentInformationManagement,
-                $quoteIdMaskFactory,
-                $cartRepository
-            );
-        }
+        parent::__construct(
+            $billingAddressManagement,
+            $paymentMethodManagement,
+            $cartManagement,
+            $paymentInformationManagement,
+            $quoteIdMaskFactory,
+            $cartRepository,
+            $logger
+        );
 
         $this->logger = $logger;
     }
